75853ef585c47116d479a3c7307bc9ae7107e9be
hauduong <duongngochau3@gmail.com>
2022-08-03 16:23:52 +0700

    edit

--- analytical_report.py
+++ analytical_report.py
@@ -10,28 +10,29 @@ def get_failure_count(path='reports/_stats.csv'):
             if each_row["Name"] == "Aggregated":
                 return each_row["Failure Count"]
 
-def get_request_count():
-    with open('reports/_stats.csv', 'r') as file:
+def get_request_count(path='reports/_stats.csv'):
+    with open(path, 'r') as file:
         reader = csv.DictReader(file)
         for each_row in reader:
             if each_row["Name"] == "Aggregated":
                 return each_row["Request Count"]
 
-if __name__ == '__main__':
-    path = sys.argv[1]
-    warning_threshold = sys.argv[2] #20
-    failure_threshold = sys.argv[3] #30
-    requests = get_request_count()
+def get_failed_percent(path):
+    requests = get_request_count(path)
     failure = get_failure_count(path)
 
     failed_percent = float(failure) / float(requests) * float(100)
 
-    if failed_percent >= float(warning_threshold):
+    if failed_percent >= float(warning_threshold):  
         status = "fail"
     elif failed_percent >= float(failure_threshold):
         status = "warning"
     else:
         status = "pass"
+    return status
 
-    print(status)
-
+if __name__ == '__main__':
+    path = sys.argv[1]
+    warning_threshold = sys.argv[2] #20
+    failure_threshold = sys.argv[3] #30
+    get_failed_percent(path)
