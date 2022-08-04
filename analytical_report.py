b9ec4167e4f2103ebe77bfaad70ebe87286d4344
hauduong <duongngochau3@gmail.com>
2022-08-04 09:42:26 +0700

    add logger

--- analytical_report.py
+++ analytical_report.py
@@ -1,6 +1,14 @@
 import csv
 from lib2to3.pytree import convert
 import sys
+import logging
+
+
+logger = logging.getLogger()
+logging.basicConfig(
+                level=logging.INFO,
+                format='%(asctime)s   %(name)-8s %(levelname)-10s %(message)s')
+
 
 
 def get_failure_count(path='reports/_stats.csv'):
@@ -21,12 +29,26 @@ def get_failed_percent(path, warning_threshold, failure_threshold):
     requests = get_request_count(path)
     failure = get_failure_count(path)
 
-    failed_percent = float(failure) / float(requests) * float(100)
+    failed_percent = round(float(failure) / float(requests) * float(100), 2)
+
+    logger.info(f"Current failed: {failed_percent}%")
+    logger.info(f"Unstable threshold: {warning_threshold}%")
+    logger.info(f"Failure threshold: {failure_threshold}%")
 
     if failed_percent >= float(warning_threshold):  
-        status = "fail"
+        logger.warning(
+            f"Current failed ({failed_percent}%) "
+            + ">= " 
+            + f"Warnings Threshold ({warning_threshold}%)")
+        logger.warning("warnings")
+        status = "warnings"
     elif failed_percent >= float(failure_threshold):
-        status = "warning"
+        logger.error(
+            f"Current failed ({failed_percent}%) "
+            + ">= " 
+            + f"Failure Threshold ({failure_threshold}%)")
+        logger.error("Unstable")
+        status = "fail"
     else:
         status = "pass"
     return status
